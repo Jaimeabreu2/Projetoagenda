69ffd926871acf448b594ada48ddb647b309e9d3
WALTER WILLIAM CAVALCANTE DE CASTRO <walter.cavalcante@aluno.uece.br>
2025-12-12 12:18:56 -0300

    error 404

--- src/main/java/br/com/agenda/controllers/Erro401Controller.java
+++ src/main/java/br/com/agenda/controllers/Erro401Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Erro401Controller {
 
     @GetMapping
-    public String mostrarTelaError() {
+    public String mostrarTelaErro() {
         return "error401";
     }
 }
